2757f8c8824f1ceab25665711dc08ebcacae3143
Jinto Jose <jintoppy@gmail.com>
2017-12-11 11:52:50 +0530

    ng-repeat

--- app.js
+++ app.js
@@ -3,8 +3,32 @@ var myapp = angular.module('myapp', []);
 myapp.controller('MyCtrl', function($scope){
     $scope.title = "AngularJS training";
     $scope.age = 10;
-    
+
     $scope.sayHi = function(){
-        alert('hi');
+        $scope.title = "modified from controller";
     };
+
+    $scope.students = [
+        {
+            name: 'Arun',
+            department: 'Arts',
+            dob: '19-01-1986'
+        },
+        {
+            name: 'Kishore',
+            department: 'Finance',
+            dob: '20-02-1987'
+        },
+        {
+            name: 'Lily',
+            department: 'Commerce',
+            dob: '25-10-1987'
+        }
+    ];
+
+    
+    $scope.onStudentSelect = function(student){
+        $scope.selectedStudent = student;
+    };
+
 });
